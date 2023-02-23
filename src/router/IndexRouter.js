7fc6628e32ca183f2efae5f64a9a5c1d2fbb5a92
news <2553039498@qq.com>
2023-02-23 20:28:29 +0800

    框架

---
+++ src/router/IndexRouter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
+import Login from '../views/login/Login';
+import NewsSandBox from '../views/sandbox/NewsSandBox';
+
+export default function IndexRouter() {
+  return (
+    <HashRouter>
+       <Switch>
+         <Route path='/login' component={Login} />
+         {/* <Route path='/' component={NewsSandBox} /> */}
+          <Route path='/'  render={()=>{
+             localStorage.getItem("token")?
+             <NewsSandBox></NewsSandBox>:
+            //  Redirect重定向 到 login组件中
+             <Redirect to='/login' />
+          }} /> 
+       </Switch>
+    </HashRouter>
+  )
+}
